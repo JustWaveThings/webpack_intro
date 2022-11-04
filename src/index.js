4d6e369ed2d25520f42c984b78b762d017e4af1b
JustWaveThings <justwavethings@proton.me>
2022-11-04 19:57:23 -0400

    Splitting bundles / output management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
 	const element = document.createElement('div');
-
+	const btn = document.createElement('button');
 	// Lodash, currently included via a script, is required for this line to work
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+	btn.textContent = 'Click me for a surprise in the console!';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
 
 	return element;
 }
